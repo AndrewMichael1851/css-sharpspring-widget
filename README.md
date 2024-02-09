513ef55ac78dbabf4f12e2c8afc7f3d44af33542
Andrew Michael <andrewmichael1851@gmail.com>
2024-02-09 11:27:57 -0500

    Added all the essential files to begin.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # css-sharpspring-widget
 A tool for integrating SharpSpring with circle S studio's WordPress website.
+
+## Project Summary
+Lorem ipsum dolor sit amet.
+
+## View Widget
+Lorem linkup dolor sit linket.
---
+++ js/app.js
@@ -0,0 +1,8 @@
+/*******************************/
+/*** SharpSpring Widget v0.1 ***/
+/*******************************/
+
+// Primary widget function.
+function sharpspringWidget() {
+    console.log("This is a test message.")
+}
