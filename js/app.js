0abea0bffb3a5003b61c363bf34dd736687683fc
Andrew Michael <andrewmichael1851@gmail.com>
2024-02-23 14:19:42 -0500

    Beginning to add the custom field options.

--- js/app.js
+++ js/app.js
@@ -2,67 +2,54 @@
 /*** SharpSpring Widget v0.1 ***/
 /*******************************/
 
-/*
-// Primary widget function.
-function sharpspringWidget() {
-    // Collected user input.
-    let webinarTitle = prompt("What is the short title of the webinar?");
-    let baseURI = prompt("Please enter your SharpSpring form baseURI.");
-    let endpoint = prompt("Please enter your SharpSpring form endpoint.");
-    let gravityFormID = prompt("Please enter the ID of your chosen Gravity Form.");
 
-    console.log(baseURI, endpoint, gravityFormID);
+/*********************/
+/*** Custom Form ***/
+/*****************/
 
-    // Format the input into the code snippet.
-    let widgetOutput = `
-    <pre><code>
+// Function to create a new field for the custom form.
+let fieldCount = 0;
+document.getElementById('newField').addEventListener('click', function(event) {
+    let newFieldSnippet = `
+        <div class="field-section">
+            <label for="gravityFormFieldID${fieldCount}">Custom Field ID:</label>
+            <input type="text" id="gravityFormFieldID${fieldCount}" name="gravityFormFieldID${fieldCount}"><br><br>
 
-// ${webinarTitle} - Webinar
-add_action( 'gform_after_submission_${gravityFormID}', 'post_to_third_party_${gravityFormID}', 10, 2 );
-
-function post_to_third_party_${gravityFormID}( $entry, $form ) {
-    $baseURI = '${baseURI}';
-    $endpoint = '${endpoint}';
-    $post_url = $baseURI . $endpoint;
-    $body = array(
-        'First Name' => rgar( $entry, '1' ),
-        'Last Name' => rgar( $entry, '3' ),
-        'Email' => rgar( $entry, '4' ),
-        'Company' => rgar( $entry, '5' ),
-        'Industry' => rgar( $entry, '7' ),
-        'trackingid__sb' => $_COOKIE['__ss_tk']
-    );
-    $request = new WP_Http();
-    $response = $request->post( $post_url, array( 'body' => $body ) );
-}
-
-    </code></pre>
+            <label for="gravityFormFieldName${fieldCount}">Custom Field Name:</label>
+            <input type="text" id="gravityFormFieldName${fieldCount}" name="gravityFormFieldName${fieldCount}"><br><br>
+        </div>
     `;
-    console.log(widgetOutput)
 
     // Print the result on the page.
-    function printResult() {
-        let result = document.getElementById("output").innerHTML = widgetOutput;
+    function addNewField() {
+        let result = document.getElementById("fieldArea").innerHTML += newFieldSnippet;
+        fieldCount += 1;
+        console.log("New Field Count: " + fieldCount);
     };
 
-    printResult()
-};
-*/
+    addNewField();
+});
 
-// Testing form submission function
-document.getElementById('widgetForm').addEventListener('submit', function(event) {
+// Custom form submission function.
+document.getElementById('customWidgetForm').addEventListener('submit', function(event) {
 
     // Prevent form submission.
     event.preventDefault();
     
     // Get form data.
-    var webinarTitle = document.getElementById('webinarTitle').value;
-    var baseURI = document.getElementById('baseURI').value;
-    var endpoint = document.getElementById('endpoint').value;
-    var gravityFormID = document.getElementById('gravityFormID').value;
+    var formTitle = document.getElementById('customFormTitle').value;
+    var baseURI = document.getElementById('customBaseURI').value;
+    var endpoint = document.getElementById('customEndpoint').value;
+    var gravityFormID = document.getElementById('customGravityFormID').value;
+
+    // TODO - add a for loop here to loop through the custom form fields.
+    for (let i = 1; i <= fieldCount; i++) {
+        // Add loop stuff here
+        console.log(i);
+    }
     
     // Print form data to console.
-    console.log('Webinar Title:', webinarTitle);
+    console.log('Webinar Title:', formTitle);
     console.log('baseURI:', baseURI);
     console.log('endpoint:', endpoint);
     console.log('gravityFormID:', gravityFormID);
@@ -71,7 +58,7 @@ document.getElementById('widgetForm').addEventListener('submit', function(event)
     let widgetOutput = `
     <pre><code>
 
-// ${webinarTitle} - Webinar
+// ${formTitle}
 add_action( 'gform_after_submission_${gravityFormID}', 'post_to_third_party_${gravityFormID}', 10, 2 );
 
 function post_to_third_party_${gravityFormID}( $entry, $form ) {
@@ -97,7 +84,66 @@ function post_to_third_party_${gravityFormID}( $entry, $form ) {
 
     // Print the result on the page.
     function printResult() {
-        let result = document.getElementById("widgetForm").innerHTML = widgetOutput;
+        let result = document.getElementById("widgetContainer").innerHTML = widgetOutput;
+    };
+
+    printResult()
+});
+
+
+/*******************/
+/*** Base Form ***/
+/***************/
+
+// Base form submission function.
+document.getElementById('widgetForm').addEventListener('submit', function(event) {
+
+    // Prevent form submission.
+    event.preventDefault();
+    
+    // Get form data.
+    var formTitle = document.getElementById('formTitle').value;
+    var baseURI = document.getElementById('baseURI').value;
+    var endpoint = document.getElementById('endpoint').value;
+    var gravityFormID = document.getElementById('gravityFormID').value;
+    
+    // Print form data to console.
+    console.log('Webinar Title:', formTitle);
+    console.log('baseURI:', baseURI);
+    console.log('endpoint:', endpoint);
+    console.log('gravityFormID:', gravityFormID);
+
+    // Format the input into the code snippet.
+    let widgetOutput = `
+    <pre><code>
+
+// ${formTitle}
+add_action( 'gform_after_submission_${gravityFormID}', 'post_to_third_party_${gravityFormID}', 10, 2 );
+
+function post_to_third_party_${gravityFormID}( $entry, $form ) {
+    $baseURI = '${baseURI}';
+    $endpoint = '${endpoint}';
+    $post_url = $baseURI . $endpoint;
+    $body = array(
+        'First Name' => rgar( $entry, '1' ),
+        'Last Name' => rgar( $entry, '3' ),
+        'Email' => rgar( $entry, '4' ),
+        'Company' => rgar( $entry, '5' ),
+        'Industry' => rgar( $entry, '7' ),
+        'trackingid__sb' => $_COOKIE['__ss_tk']
+    );
+    $request = new WP_Http();
+    $response = $request->post( $post_url, array( 'body' => $body ) );
+}
+
+    </code></pre>
+    <p>To use the form again, please reload the page.</p>
+    `;
+    console.log(widgetOutput)
+
+    // Print the result on the page.
+    function printResult() {
+        let result = document.getElementById("widgetContainer").innerHTML = widgetOutput;
     };
 
     printResult()
