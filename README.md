8de21c3b4ad3f838902a78ea226d77d5339720f9
AndrewMichael1851 <andrewmichael1851@gmail.com>
2024-03-03 22:01:25 -0500

    Finished the custom form and styling

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # css-sharpspring-widget
-A tool for integrating SharpSpring with circle S studio's WordPress website.
+A tool for integrating SharpSpring with circle S studio's WordPress website by generating a copyable PHP snippet that can be added to functions.php. When using the custom form option, this tool also allows you to integrate SharpSpring with any Gravity Form on any website.
 
 ## Project Summary
-Lorem ipsum dolor sit amet.
+SharpSpring, a leading marketing automation and CRM platform, was recently acquired by Constant Contact, who quickly began migrating content and documentation from the old platform’s pages to a new location on their own servers (acquisition in 2021, rebranding and documentation transfer in late 2023).
 
-## View Widget
+As a part of this content migration, Constant Contact broke the pages and tools my agency was using to integrate our WordPress website with SharpSpring’s CRM platform through Gravity Forms.
+
+In particular, the old documentation included a several-step process for generating a custom PHP snippet that you could paste into your site’s functions.php file to allow Gravity Form submissions to send information to SharpSpring’s servers, allowing you to update SharpSpring records automatically from form submissions on your website.
+
+This loss in documentation and the resulting breaking of the tools we were using meant that it became much more complicated to create the custom PHP snippets we needed to continue integrating forms on our website.
+
+For this reason, I built a small widget in Javascript, hosted on GitHub pages, to provide the same functionality and fill this gap in technology.
+
+You can read more about this tool and my process of making it on my blog here: https://andrewmichael1851.com/marketing-automation/sharpspring-form-integration-for-gravity-forms/.
+
+## View Widget on GitHub Pages
 https://andrewmichael1851.github.io/css-sharpspring-widget/
--- js/app.js
+++ js/app.js
@@ -2,30 +2,38 @@
 /*** SharpSpring Widget v0.1 ***/
 /*******************************/
 
+// Global Variables
+
+let fieldCount = 0;
 
 /*********************/
 /*** Custom Form ***/
 /*****************/
 
 // Function to create a new field for the custom form.
-let fieldCount = 0;
 document.getElementById('newField').addEventListener('click', function(event) {
+
+    // Add 1 to the total number of custom fields.
     fieldCount += 1;
+
+    // Declare the temporary variable for adding the HTML to the page.
     let newFieldSnippet = `
         <div class="field-section">
-            <p>Please input custom field ${fieldCount}.</p>
-            <label for="gravityFormFieldID${fieldCount}">Custom Field ID:</label>
-            <input type="text" id="gravityFormFieldID${fieldCount}" name="gravityFormFieldID${fieldCount}"><br><br>
+            <div class=field-section-item>
+                <label for="gravityFormFieldID${fieldCount}">Custom Field ${fieldCount} ID:</label>
+                <input type="text" id="gravityFormFieldID${fieldCount}" name="gravityFormFieldID${fieldCount}">
+            </div>
 
-            <label for="gravityFormFieldName${fieldCount}">Custom Field Name:</label>
-            <input type="text" id="gravityFormFieldName${fieldCount}" name="gravityFormFieldName${fieldCount}"><br><br>
+            <div class=field-section-item>
+                <label for="gravityFormFieldName${fieldCount}">Custom Field ${fieldCount} Name:</label>
+                <input type="text" id="gravityFormFieldName${fieldCount}" name="gravityFormFieldName${fieldCount}">
+            </div>
         </div>
     `;
 
-    // Print the result on the page.
+    // Function to print the result on the page.
     function addNewField() {
         let result = document.getElementById("fieldArea").innerHTML += newFieldSnippet;
-        console.log("New Field Count: " + fieldCount);
     };
 
     addNewField();
@@ -34,10 +42,10 @@ document.getElementById('newField').addEventListener('click', function(event) {
 // Custom form submission function.
 document.getElementById('customWidgetForm').addEventListener('submit', function(event) {
 
-    // Prevent form submission.
+    // Prevent default form submission.
     event.preventDefault();
     
-    // Get form data.
+    // Get base form data.
     var formTitle = document.getElementById('customFormTitle').value;
     var baseURI = document.getElementById('customBaseURI').value;
     var endpoint = document.getElementById('customEndpoint').value;
@@ -48,6 +56,7 @@ document.getElementById('customWidgetForm').addEventListener('submit', function(
     let customFieldNames = [];
 
     // Loop through the custom form fields and add the results to the above arrays.
+    // Note that i must equal 1 to align with the shift for fieldCount since it's shown on the frontend.
     for (let i = 1; i <= fieldCount; i++) {
         let tempFieldID = document.getElementById(`gravityFormFieldID${i}`).value;
         let tempFieldName = document.getElementById(`gravityFormFieldName${i}`).value;
@@ -56,7 +65,7 @@ document.getElementById('customWidgetForm').addEventListener('submit', function(
         customFieldNames.push(tempFieldName);
     };
     
-    // Print form data to console.
+    // Print form data to console for testing.
     console.log('Webinar Title:', formTitle);
     console.log('baseURI:', baseURI);
     console.log('endpoint:', endpoint);
@@ -64,12 +73,15 @@ document.getElementById('customWidgetForm').addEventListener('submit', function(
     console.log(customFieldIDs);
     console.log(customFieldNames);
 
-    // Build the Body Array
+    // Build the body array.
     let bodyArray = "";
     for (let i = 0; i < fieldCount; i++) {
-        bodyArray += `'${customFieldNames[i]}' => rgar( $entry, '${customFieldIDs[i]}' ),`
+        if (i < fieldCount - 1) {
+            bodyArray += `        '${customFieldNames[i]}' => rgar( $entry, '${customFieldIDs[i]}' ),` + "</br>";
+        } else {
+            bodyArray += `        '${customFieldNames[i]}' => rgar( $entry, '${customFieldIDs[i]}' ),`;
+        };
     };
-    console.log(bodyArray);
 
     // Format the input into the code snippet.
     let widgetOutput = `
@@ -83,12 +95,7 @@ function post_to_third_party_${gravityFormID}( $entry, $form ) {
     $endpoint = '${endpoint}';
     $post_url = $baseURI . $endpoint;
     $body = array(
-        ${bodyArray}
-        'First Name' => rgar( $entry, '1' ),
-        'Last Name' => rgar( $entry, '3' ),
-        'Email' => rgar( $entry, '4' ),
-        'Company' => rgar( $entry, '5' ),
-        'Industry' => rgar( $entry, '7' ),
+${bodyArray}
         'trackingid__sb' => $_COOKIE['__ss_tk']
     );
     $request = new WP_Http();
